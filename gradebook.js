b41ff3a984746b4f6c168fbc99b0d6c2360a4da4
sharmaragarrett <sgarrett2@my.waketech.edu>
2025-04-11 18:56:17 -0400

    Add files via upload

---
+++ gradebook.js
@@ -0,0 +1,13 @@
+// Stub: Fetch grade data from the database
+function fetchGradeData() {
+  console.log("fetchGradeData() called - will fetch data in future.");
+}
+
+// Stub: Populate the gradebook table with fetched data
+function populateGradebook(data) {
+  console.log("populateGradebook() called with data:", data);
+}
+
+// Call stubs to test
+fetchGradeData();
+populateGradebook([]);
